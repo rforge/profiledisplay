3296aa6990251f03768d232b57d8f17b92f316b8
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-07 18:21:07 +0000

    modified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/createEx.R
+++ pkg/profileDisplay/R/createEx.R
@@ -1,6 +1,7 @@
 createEx<-function(pkg=NULL){
   path <- normalizePath(getwd(), winslash="/", mustWork=FALSE)
   path<-file.path(path,paste(pkg,"-examples",sep=""))
+  if (file.exists(path)){ unlink(file.path(path), recursive=TRUE) }
   suppressWarnings(dir.create(path))
   setwd(path)
   tools:::.createExdotR(pkg, system.file(package=pkg))
