4dd7149a58b2ad2ecc280882047ea506cb981373
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-09 20:25:08 +0000

    make a small change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/createEx.R
+++ pkg/profileDisplay/R/createEx.R
@@ -4,9 +4,8 @@ createEx<-function(pkg=NULL){
   if (file.exists(path)){ unlink(file.path(path), recursive=TRUE) }
   suppressWarnings(dir.create(path))
   setwd(path)
-  tools:::.createExdotR(pkg, system.file(package=pkg))
+  tools:::.createExdotR(pkg, system.file(packpromtage=pkg))
   newpath<-normalizePath(file.path(getwd(),list.files(path)), winslash="/", mustWork=FALSE)
-  
   lines<-readLines(newpath)
   lines<-lines[!grepl("quit\\('no'\\)",lines)]
   writeLines(lines,con=newpath)
