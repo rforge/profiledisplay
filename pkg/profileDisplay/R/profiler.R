e0d0e8a633ab53260661f86e7013deebcae8c551
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-11 05:08:49 +0000

    only profiles code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/profiler.R
+++ pkg/profileDisplay/R/profiler.R
@@ -1,23 +1,7 @@
 profiler <- function(Rcode, dir = ".",pkg=NULL) {
-    path <- paste("Files for profiling",basename(Rcode),sep=" ")
-    dir.create(path)
-    if(!is.null(pkg)){
-      writeLines(getLines(Rcode,pkg=pkg)[[1]],con=file.path(path,Rcode))
-      dir <- path
-    }
-    Rprof(filename=file.path(path,"Rprof.out"), line.profiling=TRUE, memory.profiling=TRUE)
+    Rprof("Rprof.out", line.profiling=TRUE, memory.profiling=TRUE)
     source(file.path(dir,Rcode), echo=TRUE)
     Rprof(NULL)
-    oldwd <- getwd()
-    setwd(path)
-    on.exit(setwd(oldwd))
     s <- summaryRprof("Rprof.out", lines="show", memory="both")
-    d <- unique(sub("#.*","",rownames(s$by.line))[grep(".R",rownames(s$by.line))])
-    for (i in 1:length(d)){
-      writeLines(getLines(d[i],dir=dir)[[1]],con=basename(d[i]))
-    }
-    setwd(oldwd)
 return(s)
 }
-
-### runtime too short what to do?
