6f2692a8849b7de47cf38a70887297f03f035263
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-14 19:37:54 +0000

    modify a bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/createEx.R
+++ pkg/profileDisplay/R/createEx.R
@@ -6,7 +6,7 @@ createEx<-function(pkg=NULL){
   if (file.exists(path)){ unlink(file.path(path), recursive=TRUE) }
   suppressWarnings(dir.create(path))
   setwd(path)
-  tools:::.createExdotR(pkg, system.file(packpromtage=pkg))
+  tools:::.createExdotR(pkg, system.file(package=pkg))
   newpath<-normalizePath(file.path(getwd(),list.files(path)), winslash="/", mustWork=FALSE)
   lines<-readLines(newpath)
   lines<-lines[!grepl("quit\\('no'\\)",lines)]
