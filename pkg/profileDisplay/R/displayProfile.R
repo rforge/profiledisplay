876a7f3cc5f46b22474eea2f1822560be052a8d0
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-15 06:30:25 +0000

    integrates getLines,getprofFiles, selfprofileClassifier data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@103 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/profileDisplay/R/displayProfile.R
@@ -0,0 +1,85 @@
+displayprofile <- function(prof="Rprof.out", show=c("self","total","memory","executed"), data,
+                           savefile=tempdir()) {
+  dir <- dirname(prof)
+    if(dir=="."){
+      dir <- getwd()
+    }
+  otherdata <- data$other
+  data <- data[-1]
+  s <- summaryRprof(prof, lines="show", memory="both")
+  rowvals <- rownames(s$by.line)
+  doc <- names(data)
+  nums <- list()
+  q <- numeric()
+  for (i in 1:length(doc)) {
+    nums[[i]] <- grep(doc[i],rowvals)
+  }
+ h <- s$by.line[as.numeric(unlist(nums)),]
+  
+  if (savefile != tempdir()){
+    savefile <- normalizePath(savefile, winslash="/", mustWork=FALSE)
+  }
+  newpath <- savefile
+  suppressWarnings(dir.create(newpath))
+  fname <- unlist(strsplit(basename(prof), "[.]"))[1]
+  path <- file.path(newpath,paste("profileHTML", unlist(strsplit(basename(prof), "[.]"))[1]))
+  suppressWarnings(dir.create(path))
+  file <- basename(prof)
+  suppressWarnings( writeLines(readLines("/Users/apt_imac/profiling.txt"), con=file.path(path,"Rprof.out")))
+  oldwd <- getwd()
+  setwd(path)
+  on.exit(setwd(oldwd))
+
+    getprofFiles(basename(prof), dir=dir)
+  
+col <- colouring()$col
+    
+  myrenderer <- renderer_html(document=TRUE, 
+                              stylesheet=cssloc)
+  htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+  
+  names <- names(data)
+  name <- unlist(strsplit(names, "[.]"))[-grep("R",unlist(strsplit(names, "[.]")))]
+  total.sampling.time <- otherdata$total.sampling.time
+  total.time <- otherdata$total.time
+  namecode <- character()
+  if (length(names) == 1){
+    if(show == "memory"){
+      for (j in 1:length(name)){
+        namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.sampling.time," Mb)
+                             <br><i>Heavy Usage Lines</i><br>", sep="")
+      }
+      }else{
+        for (j in 1:length(name)){
+          namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.sampling.time," seconds)
+                               <br><i>Heavy Usage Lines</i><br>", sep="")
+        }
+        }
+    } else{
+      if(show == "memory"){
+        for (j in 1:length(name)){
+          namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.time[j]," Mb)
+                               <br><i>Heavy Usage Lines</i><br>", sep="")
+        }
+        }else{
+          for (j in 1:length(name)){
+            namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.time[j]," seconds)
+                                 <br><i>Heavy Usage Lines</i><br>", sep="")
+          }
+          }
+      }
+  namecode[length(namecode)+1] <- "<b><a href=\"summary.html\" target=\"list\">Summary Rprof Data</i></b>"
+  if (show == "executed"){
+    htmlize(system.file(  , package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+  }
+    if (show == "memory"){
+      htmlize(system.file("text/summarymemory.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+    }
+    if (show == "self"){
+      htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+    }
+    if (show == "total"){
+      htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+    }
+
+}
