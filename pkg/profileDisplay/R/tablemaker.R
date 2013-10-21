bc0373ba050cf73260a67df88bece7ed756ef1e8
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-21 23:31:59 +0000

    changed some of the code to be compatible with displayProfile
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/tablemaker.R
+++ pkg/profileDisplay/R/tablemaker.R
@@ -1,53 +1,51 @@
-tablemaker <- function(names,values,int=12, timeintervals, profileType, measure) {
+tablemaker <- function(lines,values,int=12, timeintervals, profileType, measure) {
   alpha <- letters[1:int]
   table <- character()
-  if (length(names) > 1){
-    moretimevals <- seq(range(values)[1], range(values)[2], length=(int-1))
-    ftimeintervals <- format(moretimevals, digits=2)
+  if (length(names(lines)) > 1){
+  #  moretimevals <- seq(range(values)[1], range(values)[2], length=(int-1))
+    ftimeintervals <- format(timeintervals, digits=2)
     timevec <- character()
     if (all(values == 0)) {
       timevec[values == moretimevals[1]] <- alpha[1]
     }else{
       for(l in 2:int){
-        timevec[values >=moretimevals[l-1] & values <moretimevals[l]] <- alpha[l]
+        timevec[values >=timeintervals[l-1] & values <timeintervals[l]] <- alpha[l]
       }
-      timevec[values == moretimevals[1]] <- alpha[1]
-      timevec[values >= moretimevals[length(moretimevals)]] <- alpha[int]
+      timevec[values == timeintervals[1]] <- alpha[1]
+      timevec[values >= timeintervals[length(timeintervals)]] <- alpha[int]
     }
     measurer <- numeric()
     for (q in 1:length(alpha)){
       measurer[q] <- sum(timevec==alpha[q])
     }
     tot<- numeric()
-    for (w in 1:length(names)){
-      tot[w] <- length(readLines(names[w])) 
+    for (w in 1:length(names(lines))){
+      tot[w] <- length(lines[[w]])
     }
     measurer[1] <- sum(tot) - sum(measurer)
     
     measurer[measurer ==0] <- "none"
-    for(l in 2:length(moretimevals)){
+  #  for(l in 2:length(timeintervals)){
+  #    table[l] <- paste("<tr><TD class=\"", alpha[l], "\"> </TD><td>", ftimeintervals[l-1], " to ",ftimeintervals[l],"</td><td>", measurer[l], "</td></tr>", sep="")
+  #  }
+  #  table[1] <- paste("<tr><TD class=\"", alpha[1], "\"> </TD><td>", ftimeintervals[1], "</td><td>", measurer[1], "</td></tr>", sep="")
+  #  table[int] <- paste("<tr><TD class=\"", alpha[int], "\"> </TD><td>", ftimeintervals[int-1],"</td><td>", measurer[int], "</td></tr>", sep="")
+  #  if(length(grep("none", table)) > 0) {
+ #     table <- table[-grep("none",table)]
+ #   }
+ #   table <- paste(table, collapse="")
+#  } else{
+    for(l in 2:length(timeintervals)){
       table[l] <- paste("<tr><TD class=\"", alpha[l], "\"> </TD><td>", ftimeintervals[l-1], " to ",ftimeintervals[l],"</td><td>", measurer[l], "</td></tr>", sep="")
     }
     table[1] <- paste("<tr><TD class=\"", alpha[1], "\"> </TD><td>", ftimeintervals[1], "</td><td>", measurer[1], "</td></tr>", sep="")
     table[int] <- paste("<tr><TD class=\"", alpha[int], "\"> </TD><td>", ftimeintervals[int-1],"</td><td>", measurer[int], "</td></tr>", sep="")
-    if(length(grep("none", table)) > 0) {
-      table <- table[-grep("none",table)]
-    }
-    table <- paste(table, collapse="")
-  } else{
-    ftimeintervals <- format(timeintervals, digits=2)
-    for(l in 2:length(timeintervals)){
-      table[l] <- paste("<tr><TD class=\"", alpha[l], "\"> </TD><td>", ftimeintervals[l-1], " to ",ftimeintervals[l],"</td><td>", measure[l], "</td></tr>", sep="")
-    }
-    table[1] <- paste("<tr><TD class=\"", alpha[1], "\"> </TD><td>", ftimeintervals[1], "</td><td>", measure[1], "</td></tr>", sep="")
-    table[int] <- paste("<tr><TD class=\"", alpha[int], "\"> </TD><td>", ftimeintervals[int-1],"</td><td>", measure[int], "</td></tr>", sep="")
     
     if(length(grep("none", table)) > 0) {
       table <- table[-grep("none",table)]
     }
-    
     table <- paste(table, collapse="")
-  }
+ # }
   if(profileType == "memory"){
     formatcode3 <- paste("</pre>\n<a name=\"Legend\"></a><h1><i>Legend</i></h1></div><div><table border=\"2\" style=\"background-color:white;\">
                              
