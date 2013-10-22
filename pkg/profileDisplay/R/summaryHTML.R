7631867ab73516b28d02fa42690af03c290fa02a
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-22 01:26:20 +0000

    converts summaryRprof to HTML
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/summaryHTML.R
+++ pkg/profileDisplay/R/summaryHTML.R
@@ -1,4 +1,13 @@
-summaryHTML <- function(prof){
-  htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", 
-          HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+summaryHTML <- function(prof,show){
+  if (show == "memory"|show == "self"|show=="total"){
+    if (show == "memory"){
+    htmlize(system.file("text/summarymemory.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+    }
+    if (show == "self"|show=="total") {
+      htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", 
+              HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
+    }
+  } else {
+    warning("Not implimented yet.")
+  }
 }
