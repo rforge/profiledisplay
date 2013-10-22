07180c275a07de6b0ee67b2aa8573c89aeea2eeb
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-22 01:07:48 +0000

    modified with some smaller functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/displayProfile.R
+++ pkg/profileDisplay/R/displayProfile.R
@@ -8,6 +8,7 @@ displayprofile <- function(prof="Rprof.out", show=c("self","total","memory","exe
   data <- data[-1]
   s <- summaryRprof(prof, lines="show", memory="both")
   loc <- rownames(s$by.line)
+  interval <- s$sample.interval
   rowvals <- rownames(s$by.line)
   k <- numeric()
   for(i in 1:length(names(lines))){
@@ -43,57 +44,15 @@ names(li) <- names(data)
   setwd(path)
   on.exit(setwd(oldwd))
 
-  #  getprofFiles(basename(prof), dir=dir)
-  
-col <- colouring()$col
-    
+col <- colouring(,int)$col 
   myrenderer <- renderer_html(document=TRUE, 
                     stylesheet=cssloc)
-  htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
-  
+summaryHTML(prof,show)
   names <- names(data)
   name <- unlist(strsplit(names, "[.]"))[-grep("R",unlist(strsplit(names, "[.]")))]
   total.sampling.time <- otherdata$total.sampling.time
-  total.time <- total.time[order(total.time, decreasing=TRUE)]
-  namecode <- character()
-  if (length(names) == 1){
-    if(show == "memory"){ ## if other profilers added, need to change, cannot be so rigid?
-      for (j in 1:length(name)){
-        namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.sampling.time," Mb)
-                             <br><i>Heavy Usage Lines</i><br>", sep="")
-      }
-      }else{
-        for (j in 1:length(name)){
-          namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.sampling.time," seconds)
-                               <br><i>Heavy Usage Lines</i><br>", sep="")
-        }
-        }
-    } else{
-      if(show == "memory"){
-        for (j in 1:length(name)){
-          namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.time[j]," Mb)
-                               <br><i>Heavy Usage Lines</i><br>", sep="")
-        }
-        }else{
-          for (j in 1:length(name)){
-            namecode[j] <- paste("<b><a href=\"", name[j], ".html\" target=\"list\">",names[j],"</a></b> (",total.time[j]," seconds)
-                                 <br><i>Heavy Usage Lines</i><br>", sep="")
-          }
-          }
-      }
-  namecode[length(namecode)+1] <- "<b><a href=\"summary.html\" target=\"list\">Summary Rprof Data</i></b>"
-  if (show == "executed"){
-    htmlize(system.file(  , package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
-  }
-    if (show == "memory"){
-      htmlize(system.file("text/summarymemory.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
-    }
-    if (show == "self"){
-      htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
-    }
-    if (show == "total"){
-      htmlize(system.file("text/summary.txt", package="profileDisplay"), "summary", HTMLdir=getwd(), title=paste("Summary Rprof Data for ",basename(prof), sep=""), local=TRUE)
-    }
+  total.time <- total.time[oldorder]
+ namecode <- namecode(show,names,otherdata)
   for ( i in 1:length(names)){
     fn1 <- fn[grep(names[i],fn)]
     ln1 <- ln[grep(names[i],fn)]
@@ -103,43 +62,9 @@ col <- colouring()$col
     colourer<- highlight(output="profile.txt", parse.output=parse(text=lines[[i]]), renderer=myrenderer, styles=data[[i]]$styles, show_line_numbers=TRUE)
     measure <- linenumbers(colourer, int=int)
     colourer <- codecondenser(readLines("profile.txt"), int=int)
-    par <- names[i]
-    if (length(names) > 1){
-      sampling.time <- total.time[i]
-      if(show =="memory"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title><body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total memory used was ", sampling.time," Mb.  Overall, the total memory used was ", total.sampling.time, " Mb.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-      if(show =="self"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title>\n<body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total elapsed time was ", sampling.time," seconds.  Overall, the total runtime was ", total.sampling.time, " seconds, using self time.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-      if(show =="total"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title>\n<body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total elapsed time was ", sampling.time," seconds.  Overall, the total runtime was ", total.sampling.time, " seconds, using total time.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-    }else{
-      if(show =="memory"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title>\n<body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total memory used was ", total.sampling.time," Mb.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-      if(show =="self"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title>\n<body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total elapsed time was ", total.sampling.time," seconds, using self time.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-      if(show =="total"){
-        formatcode <- paste("</style></head><body>\n<title>Syntax Highlighting Summary for R Profiling of " ,par, 
-                            "</title>\n<body bgcolor=\"#dddddd\"><div id=\"header\"><h1>R Profiling Summary for ", par, 
-                            "</h1>\n<p>For a sampling interval of ", interval," seconds, the total elapsed time was ", total.sampling.time," seconds, using total time.</p>\n<a name=\"Code\"></a><h1><i>Code</i></h1><pre style=\"background:",col,";border-style:double;border-width:5px;\">\n", sep="")
-      }
-    }
-    writeLines(formatcode,"format.txt")
-    form <- readLines("format.txt")
+
+    form <- headerHTML(show,names,otherdata,interval,i)
+    
     maxi <- classdeterminer(colourer,measure,int, name[i],show, unique(data[[i]][,-4])) #change to generic
     namecode[i] <- paste(namecode[i],maxi, "<br>",sep="")
     colourer[which(colourer == "</style>")] <- paste(form[1],form[2],sep="")#paste(colourer[c(which(colourer == "</style>"),which(colourer == "</head>"),which(colourer == "<body>"))],collapse="") 
@@ -153,18 +78,18 @@ col <- colouring()$col
     colourer <- c(colourer,form1)
     writeLines(colourer, paste(name[i],".html", sep=""))
   }
-  if (sum(file == names) == 0) {
+  if (sum(prof == names) == 0) {
     writeLines(paste("<html><body bgcolor=\"#dddddd\"><center><h2>Table of Contents</h2></center>", paste(namecode, collapse=""), sep=""), con=file.path(paste("profiling_nav.html", sep="" )))
-    writeLines(paste("<html><head><title>Syntax Highlighting Summary for R Profiling of " ,file, 
+    writeLines(paste("<html><head><title>Syntax Highlighting Summary for R Profiling of " ,prof, 
                      "</title></head><frameset cols=200,* border=1><frame src=\"", file.path(paste("profiling_nav.html", sep="" )), "\" name=\"nav\">
                        <frame src=\"", file.path(paste(name[1], ".html", sep="" )), "\" name=\"list\" scrolling=yes>
                        </frameset>
                        </html>", sep=""), con=file.path("Rprof.html"))
   } else{
-    writeLines(paste("<html><body bgcolor=\"#dddddd\"><center><h2>Table of Contents</h2></center>", paste(namecode, collapse=""), sep=""), con=file.path(paste(name[file == names], "_nav.html", sep="" )))
-    writeLines(paste("<html><head><title>Syntax Highlighting Summary for R Profiling of " ,file, 
-                     "</title></head><frameset cols=200,* border=1><frame src=\"", file.path(paste(name[file == names], "_nav.html", sep="" )), "\" name=\"nav\">
-                       <frame src=\"", file.path(paste(name[file == names], ".html", sep="" )), "\" name=\"list\" scrolling=yes>
+    writeLines(paste("<html><body bgcolor=\"#dddddd\"><center><h2>Table of Contents</h2></center>", paste(namecode, collapse=""), sep=""), con=file.path(paste(name[prof == names], "_nav.html", sep="" )))
+    writeLines(paste("<html><head><title>Syntax Highlighting Summary for R Profiling of " ,prof, 
+                     "</title></head><frameset cols=200,* border=1><frame src=\"", file.path(paste(name[prof == names], "_nav.html", sep="" )), "\" name=\"nav\">
+                       <frame src=\"", file.path(paste(name[prof == names], ".html", sep="" )), "\" name=\"list\" scrolling=yes>
   </frameset>
   </html>", sep=""), con=file.path("Rprof.html"))
     
@@ -172,7 +97,7 @@ col <- colouring()$col
   files <- list.files()
   keep <- c(files[grepl("html", files)], files[grepl("out", files)])
   unlink(files[-match(keep, files)], recursive=TRUE)
-  if (sum(file == names) == 0) {
+  if (sum(prof == names) == 0) {
     browseURL(url=file.path(getwd(), paste(name[1], ".html", sep="" )))
 } else{
   browseURL(url=file.path(getwd(), "Rprof.html"))
