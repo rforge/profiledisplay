0aa0dd709afaeafbb1c7b2d496a4237dce452883
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-10 15:37:21 +0000

    add new function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/profileDisplay/R/blocklines.R
@@ -0,0 +1,17 @@
+blocklines<-function(block){
+  if(block$token[1]=="IF"|block$token[1]=="WHILE"|block$token[1]=="FUNCTION"){
+    l1<-block[1,"line1"]
+    l2<-block[block$parent==block[1,"parent"] & block$token=="'('","line1"]
+    l3<-block[block$parent==block[1,"parent"] & block$token=="')'","line1"]
+    ub<-max(block$line2)
+  }else if(token$token[1]=="FOR"){
+    l1<-block[1,"line1"]
+    l2<-block[block$parent==block[block$parent==block[1,"parent"] & block$token=="forcond","id"] & block$token=="'('","line1"]
+    l3<-block[block$parent==block[block$parent==block[1,"parent"] & block$token=="forcond","id"] & block$token=="')'","line1"]
+    ub<-max(block$line2)
+  }else{l1<-l2<-l3<-min(block$line1)
+        ub<-max(block$line2)
+  }
+  return(list(l1=l1,l2=l2,l3=l3,ub=ub))
+}
+#return the line numbers of blocks
