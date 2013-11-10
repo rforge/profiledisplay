64f09c9ebae313ed6f3199b69a102bca5e17a48d
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-10 15:35:40 +0000

    add new function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/profileDisplay/R/colexp_IF.R
@@ -0,0 +1,10 @@
+colexp_IF<-function(token,ln1,class){
+  l1<-token[1,"line1"]
+  l2<-token[token$parent==token[1,"parent"] & token$token=="'('","line1"]
+  l3<-token[token$parent==token[1,"parent"] & token$token=="')'","line1"]
+  if(l2!=l3){
+    if(any(uline$line2 %in% (l2+1):l3)){
+    }else if(any(ln1 %in% c(l1:l3))) class[l1:l3]<-"1"
+  }else if(l1!=l2&&any(ln1 %in% c(l1:l3))) class[l1:l3]<-"1"
+  return(class)
+}
