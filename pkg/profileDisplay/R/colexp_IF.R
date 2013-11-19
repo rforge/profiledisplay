81b9f311a30373721d3f71718bfc37743749b2a6
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-19 17:29:54 +0000

    fix a bug about uline
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@160 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/colexp_IF.R
+++ pkg/profileDisplay/R/colexp_IF.R
@@ -2,10 +2,10 @@ colexp_IF<-function(token,ln1,class){
   l1<-token[1,"line1"]
   l2<-token[token$parent==token[1,"parent"] & token$token=="'('","line1"]
   l3<-token[token$parent==token[1,"parent"] & token$token=="')'","line1"]
-  uline<-token[token$line1%in%(l2:l3)&token$line2%in%(l2:l3),]
+  uline<-token[token$line1!=token$line2&token$line1%in%(l2:l3)&token$line2%in%(l2:l3),]
   uline<-blocksFliter(uline,d,ln1)$uline
   if(l2!=l3){
-    if(nrow(uline!=0)){
+    if(nrow(uline)!=0){
     }else if(any(ln1 %in% c(l1:l3))) class[l1:l3]<-"1"
   }else if(l1!=l2&&any(ln1 %in% c(l1:l3))) class[l1:l3]<-"1"
   return(class)
