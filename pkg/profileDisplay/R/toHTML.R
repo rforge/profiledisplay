a243d587dc03a8280d8ef63e3c5996ffe163d5b4
murdoch <murdoch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-25 01:55:40 +0000

    Add contents listing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/toHTML.R
+++ pkg/profileDisplay/R/toHTML.R
@@ -5,12 +5,13 @@ toHTML.lineClassifier <- function(x, htmldir = tempdir(), ...) {
     files <- x$files
     titles <- x$titles
     info <- x$info
+    filenames <- names(files)
+    htmlfilenames <- paste0(filenames, ".html")
     
-    allclasses <- c()
-    
+    allclasses <- c()    
     for (i in seq_along(files)) {
-	name <- names(files)[i]
-	htmlcon <- file( file.path(htmldir, paste0(name, ".html")), "wt")
+	name <- filenames[i]
+	htmlcon <- file( file.path(htmldir, htmlfilenames[i]), "wt")
 	on.exit(close(htmlcon))
 	cat(file = htmlcon,
 	    '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">',
@@ -19,7 +20,16 @@ toHTML.lineClassifier <- function(x, htmldir = tempdir(), ...) {
 	    '<link rel="stylesheet" type="text/css" href="profileDisplay.css">',
 	    '</head>',
 	    '<body>',
-	    paste0('<h1>', titles[i], '</h1>'),
+	    '<div id="header">',
+	    paste0('<h1 style="margin-bottom:0">', titles[i], '</h1>'),
+	    '</div>',
+	    '<div id="TOC" style="width:100px; float:left;">',
+	    '<br>',
+	    '<b>Files</b><br>',
+	    paste0('<a href="', htmlfilenames, '">', filenames, '</a><br>'),
+	    '</div>',
+	    '<div id="listing">',
+	    '<br>',
 	    info[i],
 	    paste0('<h2>Code</h2>'),
 	    '<pre>',
@@ -28,8 +38,8 @@ toHTML.lineClassifier <- function(x, htmldir = tempdir(), ...) {
 	lines <- files[[i]]$lines
 	classes <- files[[i]]$styles
 	allclasses <- unique(c(allclasses, classes))
-	
-	linenum <- sprintf("%0*d  ", floor(log10(length(lines))) + 1, seq_along(lines))
+        linenum <- files[[i]]$line1
+	linenum <- sprintf("%0*d  ", floor(log10(max(linenum))) + 1, linenum)
 	regexp <- "(^\\s*)(\\S*.*\\S*)(\\s*$)"
 	pre <- sub(regexp, "\\1", lines)
 	mid <- sub(regexp, "\\2", lines)
@@ -42,11 +52,12 @@ toHTML.lineClassifier <- function(x, htmldir = tempdir(), ...) {
 	    
 	cat(file = htmlcon,
 	    '</pre>',
+	    '</div>',
 	    '</body></html>',
 	    '', sep="\n")
 	close(htmlcon)
 	on.exit()
     }
     writeCSS(sort(allclasses), file.path(htmldir, "profileDisplay.css"), ...)
-    htmldir
+    file.path(htmldir, htmlfilenames[1])
 }
--- pkg/profileDisplay/R/writeCSS.R
+++ pkg/profileDisplay/R/writeCSS.R
@@ -1,7 +1,8 @@
 writeCSS <- function(classes,
                       filename,
                       bg = c("white" ,"#FED98E"), 
-		      fg = c("black", "black")) {
+		      fg = c("black", "black"),
+		      border.bg = "#FED98E") {
     n <- length(classes)
     bg <- rgb(colorRamp(bg, space = "Lab")(seq(0, 1, len=n)), maxColorValue=255)
     fg <- rgb(colorRamp(fg, space = "Lab")(seq(0, 1, len=n)), maxColorValue=255)
@@ -10,7 +11,9 @@ writeCSS <- function(classes,
     on.exit(close(con))
     cat(file = con,
 	paste0(".", classes, '{background-color:', bg, '; color:', fg, ';}'),
-	'.line{color:black;}',
+	paste0('#header{background-color:', border.bg, ';}'),
+	paste0('#TOC{background-color:', border.bg, '; height:90%; float:left;}'),
+	'#listing{margin-left:20px; float:left;}',
 	'', sep = "\n")
     filename
 }
