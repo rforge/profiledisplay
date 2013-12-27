c97e6bfbbbaa3f2c5bd724c4d40c4130ae13c8c5
atsang48 <atsang48@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-27 19:55:59 +0000

    replaced render_html with writeCSS
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/displayProfile.R
+++ pkg/profileDisplay/R/displayProfile.R
@@ -3,7 +3,7 @@ displayprofile <- function(prof="Rprof.out",
                            #lines=getLines(c("Example.R","Example1.R","Example2.R"),"/Users/apt_imac"), #modify
                            data=selfprofileClassifier(prof,src = listRfiles(prof, dir=".")), #in this classifier can it only save self/total/memory that it needs?
                            otherdata= profiledata(prof,lines),
-                           int=12,
+                           breaks=12,
                           savefile=tempdir(), colourdata = colouring()$colourdata) {
   dir <- dirname(prof)
   if(dir=="."){
@@ -42,18 +42,19 @@ names(li) <- names(files)
   }
   newpath <- savefile
   suppressWarnings(dir.create(newpath))
-  fname <- unlist(strsplit(basename(prof), "[.]"))[1]
-  path <- file.path(newpath,paste("profileHTML", unlist(strsplit(basename(prof), "[.]"))[1]))
-  suppressWarnings(dir.create(path))
   file <- basename(prof)
+  fname <- unlist(strsplit(file, "[.]"))[1]
+  path <- file.path(newpath,paste("profileHTML", unlist(strsplit(file, "[.]"))[1]))
+  suppressWarnings(dir.create(path))
+
   suppressWarnings( writeLines(readLines(file.path(dir,prof)), con=file.path(path,"Rprof.out")))
   oldwd <- getwd()
   setwd(path)
   on.exit(setwd(oldwd))
 
-col <- colouring(,int)$col 
-  myrenderer <- renderer_html(document=TRUE, 
-                    stylesheet=cssloc)
+col <- colouring(,breaks)$col 
+  myrenderer <- writeCSS(letters[1:12],"test.css",bg=c("#FFFFD4" ,"#FED98E")) #replace letters later with more generic classes (p01...?)
+  
 summaryHTML(prof,show)
   names <- names(data)
   name <- unlist(strsplit(names, "[.]"))[-grep("R",unlist(strsplit(names, "[.]")))]
@@ -73,21 +74,21 @@ summaryHTML(prof,show)
       test<-  data[[i]][rep(q,sum(p==unique(p)[q])),]
       test1 <- rbind(test1,test)
     }
-    timeintervals <- seq(range(values)[1], range(values)[2], length=(int-1))
+    timeintervals <- seq(range(values)[1], range(values)[2], length=(breaks-1))
     colourer <- character()
     colourer<- highlight(output="profile.txt", parse.output=parse(text=lines[[i]]), renderer=myrenderer, styles=test1$styles, show_line_numbers=TRUE)
-  measure <- linenumbers(colourer, int=int)
-    colourer <- codecondenser(readLines("profile.txt"), int=int)
+  measure <- linenumbers(colourer, int=breaks)
+    colourer <- codecondenser(readLines("profile.txt"), int=breaks)
 
     form <- headerHTML(show,names,otherdata,interval,i)
     
-    maxi <- classdeterminer(colourer,measure,int, name[i],show, data[[i]]) #change to generic
+    maxi <- classdeterminer(colourer,measure,breaks, name[i],show, data[[i]]) #change to generic
     namecode[i] <- paste(namecode[i],maxi, "<br>",sep="")
     colourer[which(colourer == "</style>")] <- paste(form[1],form[2],sep="")#paste(colourer[c(which(colourer == "</style>"),which(colourer == "</head>"),which(colourer == "<body>"))],collapse="") 
     colourer[which(colourer == "</head>")] <- form[3]
     colourer[which(colourer == "<body>")] <- form[4]
     colourer[which(colourer == "<pre>")] <- form[5]
-    formatcode3 <- tablemaker(lines,values,int, timeintervals, show, measure)
+    formatcode3 <- tablemaker(lines,values,breaks, timeintervals, show, measure)
     writeLines(formatcode3,"format1.txt") #easy way to do this without creating a file?
     form1 <- readLines("format1.txt")
     colourer <- colourer[-((length(colourer)-2):length(colourer))]
