1a21158f0dcdccf8a3dc5a217afc7bb0310e285f
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-08 15:43:39 +0000

    rename the function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/profileDisplay/R/blocksFilter.R
@@ -0,0 +1,17 @@
+blocksFilter<-function(uline=NULL,d=NULL,ln1=NULL){
+  if(nrow(uline)!=0){
+    num<-NULL
+    for(i in 1:nrow(uline)){
+      token<-d[d$id %in% descendant(data=d,id=uline[i,"id"]),"token"]
+      if(token[1] %in% c("IF","FUNCTION","FOR","WHILE","REPEAT")|(token[1]=="'{'"&token[length(token)]=="'}'")){
+      }else{
+        num<-c(num,i)
+        if(!any(token %in% c("IF","FUNCTION","FOR","WHILE","REPEAT","'{'"))&&any(ln1 %in% c(uline$line1[i]:uline$line2[i]))){
+          ln1<-unique(sort(c(ln1,uline$line1[i]:uline$line2[i])))
+        }
+      }
+    }
+    if(!is.null(num))  uline<-uline[-num,]
+  }
+  return(list(uline=uline,ln1=ln1))
+}
