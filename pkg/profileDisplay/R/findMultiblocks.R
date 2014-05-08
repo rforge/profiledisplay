2448b626bf3b375599cb151bf17b72ed6169fe79
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-08 15:34:48 +0000

    fix a typo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/findMultiblocks.R
+++ pkg/profileDisplay/R/findMultiblocks.R
@@ -7,7 +7,7 @@ findMultiblocks<-function(s=NULL,d,filename=NULL){
   fn <-fn[which(fn==basename(filename))]
   ln1<-sort(as.numeric(ln[which(ln != "")]))
   uline<-d[d$line1!=d$line2,]
-  list<-blocksFliter(uline,d,ln1)
+  list<-blocksFilter(uline,d,ln1)
   uline<-list$uline;ln1<-list$ln1
   class[ln1]<-"1"
   if(nrow(uline)!=0){
