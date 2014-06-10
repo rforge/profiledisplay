5fef00af0cf41410d5043344aa20ae53d5872df0
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-10 18:34:23 +0000

    fix the bug of lacking argument d
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/colorexpand.R
+++ pkg/profileDisplay/R/colorexpand.R
@@ -2,9 +2,9 @@ colorexpand<-function(uline=NULL,d=NULL,ln1=NULL,class){
   for(i in 1:nrow(uline)){
     token<-d[d$id %in% descendant(data=d,id=uline[i,"id"]),]
     if(token$token[1]=="IF"|token$token[1]=="WHILE"|token$token[1]=="FUNCTION"){
-      class<-colexp_IF(token,ln1,class)
+      class<-colexp_IF(token,d,ln1,class)
     }else if(token$token[1]=="FOR"){
-      class<-colexp_FOR(token,ln1,class)
+      class<-colexp_FOR(token,d,ln1,class)
     }
   }
   ln1<-c(1:length(class))[class=="1"]
