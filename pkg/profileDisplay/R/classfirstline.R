d5cb13f607ea7d6edfd2925fc8b528077fbb3791
yxie87 <yxie87@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-01 02:39:12 +0000

    revise the bug with <-
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/profiledisplay@252 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/profileDisplay/R/classfirstline.R
+++ pkg/profileDisplay/R/classfirstline.R
@@ -3,7 +3,7 @@ classfirstline<-function(class,uline,d,ln1){
   for(i in 1:nrow(uline)){
     block<-d[d$id %in% descendant(data=d,id=uline[i,"id"]),]
     lines<-blocklines(block)
-    l1<-lines$l1;l2<-lines$l2;l3<-lines$l3;ub<lines$ub
+    l1<-lines$l1;l2<-lines$l2;l3<-lines$l3;ub<-lines$ub
     if(max(ln1)>=ub){
       class[l1:l3]<-"2"
     }else{
